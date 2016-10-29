6b33e6f1058ca7e482a1c31c197d0eea49f23c29
Jesse Smith <jesse.smth09@gmail.com>
2016-10-28 22:39:02 -0400

    Added Prority Queue

---
+++ Elevator_System.py
@@ -0,0 +1,19 @@
+from queue import PriorityQueue
+
+class Elevator_System:
+    def __init__(self):
+        self.rQueue = PriorityQueue()
+
+    def addRequest(self, priority, data):
+        self.rQueue.put(priority, data)
+
+    def getRequest(self):
+       self.rQueue._get()
+
+    def isEmpty(self):
+        if(self.rQueue.empty()):
+            return True
+        return False
+
+    def getRequestSize(self):
+        return self.rQueue.qsize()
