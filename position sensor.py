b5e7295a20ccebd9c9ca9e3acc651458d668c1ee
Essa <eaa6@hood.edu>
2016-11-10 20:05:48 -0500

    add new sensor

--- position sensor.py
+++ position sensor.py
@@ -1,7 +1,7 @@
 
 class position_sensor:
     def _init_(self):
-        self.Difference_In_Position = 0.5; # 0.5 centimeter
+        self.Difference_In_Position = 0.5; # the differeance between the elevator Surface and the floor surface mesurment in 0.5 centimeter
 
     def is_Parallel (self):
         if self.Difference_In_Position <= 0.5:
