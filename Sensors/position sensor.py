b6894464b74191eebbfe3b7c91303e884c23f4e6
Jesse Smith <jesse.smth09@gmail.com>
2016-11-14 17:34:09 -0500

    Added Door and Position Sensor

--- Sensors/position sensor.py
+++ Sensors/position sensor.py
@@ -1,9 +1,9 @@
-
-class position_sensor:
+from Sensors.Sensor import Sensor
+class position_sensor(Sensor):
     def _init_(self):
         self.Difference_In_Position = 0.5; # the differeance between the elevator Surface and the floor surface mesurment in 0.5 centimeter
 
-    def is_Parallel (self):
+    def isSafe(self):
         if self.Difference_In_Position <= 0.5:
             return True
         else:
