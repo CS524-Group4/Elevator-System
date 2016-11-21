e169d9864fe99b35134c43223c0207632b965624
Jesse Smith <jesse.smth09@gmail.com>
2016-11-21 17:18:40 -0500

    Updated Elevator System

--- Elevator_System.py
+++ Elevator_System.py
@@ -13,9 +13,9 @@ class ElevatorSystem:
     def add_request(self, request, floor, user):
         if user == "passenger":
             new_request = Request(2, request, floor, user)
-        if user == "operator":
+        elif user == "operator":
             new_request = Request(1, request, floor, user)
-        if user == "firefighter":
+        elif user == "firefighter":
             new_request = Request(0, request, floor, user)
         self.rQueue.put(new_request)
 
@@ -42,16 +42,36 @@ class ElevatorSystem:
         if self.is_safe():
             self.close_door()
             self.car.move_car(floor)
-            #add check position sensor
+            self.check_sensor(self, "position")
             self.open_door()
 
     def open_door(self):
-        #check position sensor
+        self.check_sensor(self, "position")
         self.car.door_open()
 
     def close_door(self):
-        #add check position sensor
+        self.check_sensor(self, "door")
         self.car.door_close()
 
     def is_safe(self):
         safe = self.sensors.check_all_sensors()
+
+    def check_sensor(self, sensor):
+
+        measure_sensor = sensor
+
+        if measure_sensor == "position":
+            safe = self.sensors.get_pos().isSafe()
+        elif measure_sensor == "door":
+            safe = self.sensors.get_door().isSafe()
+        elif measure_sensor == "weight":
+            safe = self.sensors.get_weight().isSafe()
+
+        if not safe:
+            return False;
+        else: return True;
+
+
+
+
+
